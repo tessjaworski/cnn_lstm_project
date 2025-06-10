b40fc7ea478168bbee5b518785640c6e8c0dadf8
tessjaworski <tessjaworski@ufl.edu>
2025-06-10 14:57:47 -0400

    quick fix

--- model.py
+++ model.py
@@ -47,7 +47,7 @@ class HybridCNNLSTM(nn.Module):
         self.pred_steps = pred_steps
         # Merge both processed inputs
         self.combined_fc = nn.Sequential(
-            nn.Linear(lstm_hidden * 2, 512),
+            nn.Linear(lstm_hidden + 64, 512),
             nn.Dropout(0.1),
             nn.ReLU(),
             nn.Linear(512, zeta_nodes * pred_steps)
