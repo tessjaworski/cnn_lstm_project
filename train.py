fe10f660930e64f5a2d3ad7c0c1e547c05691150
tessjaworski <tessjaworski@ufl.edu>
2025-07-02 14:18:16 -0400

    small change

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ class StormSurgeDataset(data.Dataset):
 era5_mm, cora, tr_idx, va_idx, test_idx, mask_np, μ_cora, σ_cora = load_dataset()
 
 # Build kNN graph over your CORA node coordinates
-coords     = load_cora_coordinates(CORA_PATH, mask_np)     
+coords     = load_cora_coordinates(CORA_PATHS, mask_np)     
 edge_index = build_edge_index(coords, k=8).to(device)       
 
 
